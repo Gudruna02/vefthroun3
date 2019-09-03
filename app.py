2058bf77392e0f07ce8fe4b3619bcf641721956e
Gudruna02 <gapalsd@gmail.com>
2019-09-03 16:52:50 +0000

    commit

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Flask er flottast</h1>'
+	return '<h1>Flask er flottast, rosa gaman</h1><a href="/sida1">Síða 1</a><a href="/sida2">Síða 2</a>'
+
+@app.route('/sida1')
+def sida1():
+	return '<h1>Síða 1</h1><a href="/">Heim</a><a href="/sida1">Síða 1</a><a href="/sida2">Síða 2</a>'
+
+@app.route('/sida2')
+def sida2():
+	return '<h1>Síða 2</h1><a href="/">Heim</a><a href="/sida1">Síða 1</a><a href="/sida2">Síða 2</a>'
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
